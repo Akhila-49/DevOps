d1cae5af9fac0af36b8a57060d7de95271d295d4
Akhila-49 <85725678+Akhila-49@users.noreply.github.com>
2022-12-16 15:22:38 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -46,19 +46,11 @@ resource "aws_route_table_association" "project-rt-association" {
 
 resource "aws_security_group" "project-security-group" {
 
-    name = "project-security-group"
     vpc_id = aws_vpc.project-vpc.id
     ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
